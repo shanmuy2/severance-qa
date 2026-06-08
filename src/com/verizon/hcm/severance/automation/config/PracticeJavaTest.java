6c3fc7d3876b159eee84a6aa2685229088f2ac2d
shanmuy2 <samplegit1@gmail.com>
2026-06-08 13:08:01 +0530

    git java

---
+++ src/com/verizon/hcm/severance/automation/config/PracticeJavaTest.java
@@ -0,0 +1,16 @@
+package com.verizon.hcm.severance.automation.config;
+
+public class PracticeJavaTest {
+
+	private void firstMethod() {
+		System.out.println("First Method is executed testing is done");
+		System.out.println("First Method is executed testing 1 is done");
+		System.out.println("First Method is executed testing 2 is done");
+		System.out.println("First Method is executed testing 3 is done");
+	}
+
+	public static void main(String[] args) {
+		PracticeJavaTest objectName = new PracticeJavaTest();
+		objectName.firstMethod();
+	}
+}
